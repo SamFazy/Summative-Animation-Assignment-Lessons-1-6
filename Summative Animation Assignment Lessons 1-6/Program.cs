a3bcda44b19cedffe09b1e920888500badc12314
Sam F <samfaz10@hotmail.com>
2021-12-03 12:04:47 -0500

    Add project files.

---
+++ Summative Animation Assignment Lessons 1-6/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Summative_Animation_Assignment_Lessons_1_6
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Game1())
+                game.Run();
+        }
+    }
+}
